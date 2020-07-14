3493a95bd449eadc0b499af507c66f2428af801c
clmitchell630 <clmitchell630@gmail.com>
2020-07-14 05:45:46 -0500

    created portfolio cards and designed layout for portfolio

--- client/src/components/BootContent/index.js
+++ client/src/components/BootContent/index.js
@@ -1,27 +1,44 @@
 import React, { Component } from "react";
 import project from "../../project.json";
+import Card from "../Card";
 
 import "./bootcontent.scss";
+import burger from "../../utils/img/burger.PNG";
 
 class BootContent extends Component {
     state = {
-        portCard: project
+        burgers: burger
     }
     componentDidMount() {
         window.scrollTo(0, 0)
     }
     render() {
         return (
-            <main>
-                <h2>
-                    Under Construction
-                </h2>
-                <ul>
-                    <li>
-
-                    </li>
-                </ul>
-            </main>
+            <section>
+                <div>
+                    <Card
+                        img={this.state.burgers}
+                        title="BurgerEater"
+                        description="Simple burger eating app that makes you hungry while playing! Utilizes MySQL, Node.js, npm Express, npm Handlebars, npm mysql, and utilizes Heroku to host. BurgerBurger is an exercise in creating a webapp with a server and a database."
+                        link="https://burgerlist-sql.herokuapp.com/"
+                        gitLink="https://github.com/clmitchell630/burger"
+                    />
+                    <Card
+                        img={this.state.burgers}
+                        title="BurgerEater"
+                        description="Simple burger eating app that makes you hungry while playing! Utilizes MySQL, Node.js, npm Express, npm Handlebars, npm mysql, and utilizes Heroku to host. BurgerBurger is an exercise in creating a webapp with a server and a database."
+                        link="https://burgerlist-sql.herokuapp.com/"
+                        gitLink="https://github.com/clmitchell630/burger"
+                    />
+                    <Card
+                        img={this.state.burgers}
+                        title="BurgerEater"
+                        description="Simple burger eating app that makes you hungry while playing! Utilizes MySQL, Node.js, npm Express, npm Handlebars, npm mysql, and utilizes Heroku to host. BurgerBurger is an exercise in creating a webapp with a server and a database."
+                        link="https://burgerlist-sql.herokuapp.com/"
+                        gitLink="https://github.com/clmitchell630/burger"
+                    />
+                </div>
+            </section>
         );
     }
 }
