6671110f8937c58bcbad289912c26c4851a160e3
clmitchell630 <clmitchell630@gmail.com>
2020-07-14 21:50:24 -0500

    added images and finished data for portfolio

--- client/src/utils/data/data.js
+++ client/src/utils/data/data.js
@@ -1,86 +1,93 @@
 const data = [
     {
+        "title": "Looking For Group",
+        "link": "https://lookingforgroup-app.herokuapp.com/",
+        "gitLink": "https://github.com/clmitchell630/looking-for-group",
+        "img": require ("../../utils/img/portimg/lfg.PNG"),
+        "description": "We created an app for individuals who enjoy playing video games and are looking for other’s who share the same enjoyment. Our app, “Looking for Group”, is intended to match up individuals with one another and create friendships through video games."
+    },
+    {
         "title": "Wiki-Werks",
-        "link": "wiki-werks",
-        "gitLink": "wiki-werks",
+        "link": "https://clmitchell630.github.io/wiki-werks/",
+        "gitLink": "https://github.com/clmitchell630/wiki-werks",
         "img": require ("../../utils/img/portimg/scrape.PNG"),
         "description": "First class project. Worked with 3 other classmates to put together a web app. This app takes a search topic and pulls up pages from Wikipedia and vtitleeos from YouTube APIs using the AJAX call and saves your topics to bring up later."
     },
     {
         "title": "Rorschach Clicker",
-        "link": "clicky-game",
-        "gitLink": "clicky-game",
+        "link": "https://clmitchell630.github.io/clicky-game/",
+        "gitLink": "https://github.com/clmitchell630/clicky-game",
         "img": require ("../../utils/img/portimg/clicky.PNG"),
         "description": "A memory game made with React where you try to click all the images and gain points! If you click the same image twice, you lose... made especially difficult because the images are Rorschach inkblots! "
     },
     {
         "title": "News'Crepe",
         "link": "https://news-crepe.herokuapp.com/",
-        "gitLink": "news-s-crepe",
+        "gitLink": "https://github.com/clmitchell630/news-s-crepe",
         "img": require ("../../utils/img/portimg/scrape.PNG"),
         "description": "A gaming news app that scrapes articles from pcgamer to help sort what you want to read!"
     },
     {
         "title": "BurgerBurger",
-        "link": "https://whispering-falls-89513.herokuapp.com/",
-        "gitLink": "burger",
+        "link": "https://burgerlist-sql.herokuapp.com",
+        "gitLink": "https://github.com/clmitchell630/burger",
         "img": require ("../../utils/img/portimg/burger.PNG"),
         "description": "Simple burger eating app that makes you hungry while playing! Utilizes MySQL, Node.js, npm Express, npm Handlebars, npm mysql, and utilizes Heroku to host. BurgerBurger is an exercise in creating a webapp with a server and a database."
     },
     {
         "title": "Friendspace",
         "link": "https://fierce-caverns-64082.herokuapp.com/",
-        "gitLink": "friend-finder",
-        "img": require ("../../utils/img/portimg/scrape.PNG"),
+        "gitLink": "https://github.com/clmitchell630/friend-finder",
+        "img": require ("../../utils/img/portimg/friendfind.PNG"),
         "description": "This webapp helps you find new friends!.. sorta. Using Node.js and Heroku to host, Friendspace is an exercise in creating a webapp with a server."
     },
     {
         "title": "Bamazon",
-        "link": "https://hub.com/clmitchell630/product-manager",
-        "gitLink": "product-manager",
-        "img": require ("../../utils/img/portimg/scrape.PNG"),
+        "link": "https://www.youtube.com/watch?v=vHZxqrzQhx4&feature=youtu.be",
+        "gitLink": "https://github.com/clmitchell630/product-manager",
+        "img": require ("../../utils/img/portimg/bamazon.PNG"),
         "description": "This app utilizes CLI, Node.js, and mySQL to allow the user to view a list of available items, select which one they'd like to 'purchase', and select the quantity they'd like. Doing so will let the user know the quantity of the item they 'purchased' and the total price of the transaction."
     },
     {
         "title": "Liri Bot",
-        "link": "https://github.com/clmitchell630/liri-bot",
-        "gitLink": "liri-bot",
-        "img": require ("../../utils/img/portimg/scrape.PNG"),
+        "link": "https://www.youtube.com/watch?v=s076Wo2j3Os&feature=youtu.be",
+        "gitLink": "https://github.com/clmitchell630/liri-bot",
+        "img": require ("../../utils/img/portimg/liri.PNG"),
         "description": "By inputting your your favorite artist into liri you can find the venues that they are playing in, where those venues are, and what date/time the event is occuring. If you give liri a track, it will list songs from a variety of arists that meet your search term and give you a link to listen to the song. Finally, if you give liri a movie title, it will list information about the movie from where it was produced to what the ratings for the movie were."
     },
     {
         "title": "Where's My Train",
-        "link": "train-scheduler",
-        "gitLink": "train-scheduler",
-        "img": require ("../../utils/img/portimg/scrape.PNG"),
+        "link": "https://clmitchell630.github.io/train-scheduler/",
+        "gitLink": "https://github.com/clmitchell630/train-scheduler",
+        "img": require ("../../utils/img/portimg/trains.PNG"),
         "description": "A practice in Firebase, pushing information to the Firebase server and calling that information back to the app. Also utilizes Moment.js to calculate times."
     },
     {
         "title": "Gif in a Jiff",
-        "link": "gif-in-a-jiff",
-        "gitLink": "gif-in-a-jiff",
-        "img": require ("../../utils/img/portimg/scrape.PNG"),
+        "link": "https://clmitchell630.github.io/gif-in-a-jiff/",
+        "gitLink": "https://github.com/clmitchell630/gif-in-a-jiff",
+        "img": require ("../../utils/img/portimg/gifjiff.PNG"),
         "description": "A practive in AJAX calls using the Giphy API to create a page of .gif images responding to a topic stored in a button. Default buttons are provtitleed but you can add more by adding them to the 'MORE GIFS!' field."
     },
     {
         "title": "TV Trivia Time!",
-        "link": "TriviaGame",
-        "gitLink": "TriviaGame",
-        "img": require ("../../utils/img/portimg/scrape.PNG"),
+        "link": "https://clmitchell630.github.io/TriviaGame/",
+        "gitLink": "https://github.com/clmitchell630/TriviaGame",
+        "img": require ("../../utils/img/portimg/trivia.PNG"),
         "description": "A game where you answer timed tv trivia questions!"
     },
     {
         "title": "RupeeCollector",
-        "link": "unit-4-game",
-        "gitLink": "unit-4-game",
-        "img": require ("../../utils/img/portimg/scrape.PNG"),
+        "link": "https://clmitchell630.github.io/unit-4-game/",
+        "gitLink": "https://github.com/clmitchell630/unit-4-game",
+        "img": require ("../../utils/img/portimg/rupee.PNG"),
         "description": "A game where you add rupees together to match the randomly generated number. Careful though, you don't know how much each gem is worth and their values change at the begining of each game."
     },
     {
         "title": "League of Legends - Hangman",
-        "link": "Word-Guess-Game",
-        "gitLink": "Word-Guess-Game",
-        "img": require ("../../utils/img/portimg/scrape.PNG"),
+        "link": "https://clmitchell630.github.io/Word-Guess-Game/",
+        "gitLink": "https://github.com/clmitchell630/Word-Guess-Game",
+        "img": require ("../../utils/img/portimg/champ.PNG"),
         "description": "Vanilla JavaScript hangman game with a League of Legends theme!"
     }
 ]
