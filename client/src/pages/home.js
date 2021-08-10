400bf0209a99f05ee5065a51dc4cb9b6bc783c36
clmitchell630 <50003943+clmitchell630@users.noreply.github.com>
2021-08-09 19:33:51 -0500

    new main page work started

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,21 +1,12 @@
 import React from "react";
-import Header from "../components/Header";
-import About from "../components/About";
-import Tech from "../components/Tech";
-import Portfolio from "../components/Portfolio";
-
 import Nav from "../components/Nav";
-import Footer from "../components/Footer";
+import Front from "../components/Front";
 
 function Home() {
     return (
         <div>
-            <Nav color="green"/>
-            <Header/>
-            <About />
-            <Tech />
-            <Portfolio />
-            <Footer />
+            <Front/>
+
         </div>
     )
 }
