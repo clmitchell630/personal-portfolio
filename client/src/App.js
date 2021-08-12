47c820f8abc42a87b9d9c1776516784b93250d94
clmitchell630 <50003943+clmitchell630@users.noreply.github.com>
2021-08-12 07:06:24 -0500

    more pruning and compiling

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,9 @@
 //production version
 
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Home from "./pages/home";
-import Unknown from "./pages/unknown";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 
+import pageNoFind from "./utils/img/404.jpg";
 class App extends Component {
 
   render() {
@@ -23,3 +22,75 @@ class App extends Component {
 }
 
 export default App;
+
+//-----------
+//PAGES
+//-----------
+
+function Home() {
+  return (
+      <div>
+          <Links />
+          <Front />
+      </div>
+  )
+}
+
+function Unknown() {
+  return (
+      <NotFound />
+  );
+}
+
+//-----------
+//COMPONENTS
+//-----------
+
+function Front() {
+  return (
+      <div className="section">
+          <div>
+              <h1>
+                  Chris
+              </h1>
+              <h1>
+                  Mitchell
+              </h1>
+              <h2>
+                  Full-Stack Web Developer
+              </h2>
+          </div>
+      </div>
+  );
+}
+
+function Links() {
+  return (
+      <ul className="links-container">
+          <li>
+              <a href="https://www.linkedin.com/in/clmitchell630/" target="_blank" rel="noopener noreferrer" className="link-button">
+                  <i class="fab fa-linkedin-in"></i>
+              </a>
+          </li>
+          <li>
+              <a href="https://www.github.com/clmitchell630/" target="_blank" rel="noopener noreferrer" className="link-button">
+                  <i class="fab fa-github"></i>
+              </a>
+          </li>
+          {/* <div>
+              <a>
+                  <i class="fas fa-at"></i>
+              </a>
+          </div> */}
+      </ul>
+  )
+}
+
+function NotFound() {
+  return (
+      <main className="not-found">
+          <img src={pageNoFind} alt="404 Page not Found" />
+          <Link to="/"><h5>Back to home</h5></Link>
+      </main>
+  )
+}
